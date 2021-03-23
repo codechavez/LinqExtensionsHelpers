517b028a778188ff4913a85c46f1769b4c410cbd
Victor A Chavez <v.a.ch.m@hotmail.com>
2021-03-23 16:29:17 -0700

    First Commit

---
+++ CodeChavez.LinqExtensions/CodeChavez.LinqExtensions/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeChavez.LinqExtensions
+{
+    public static class Helpers
+    {
+        public static IEnumerable<TSource> Except<TSource, TKey>(this IEnumerable<TSource> incoming, IEnumerable<TSource> original, Func<TSource, TKey> comparingKeys)
+        {
+            if (incoming == null) throw new ArgumentNullException($"incoming list cannot be null - {nameof(incoming)}");
+            if (original == null) throw new ArgumentNullException($"original list cannot be null - {nameof(original)}");
+            if (comparingKeys == null) throw new ArgumentNullException($"Lambda cannot be null = {nameof(comparingKeys)}");
+
+            var keys = new HashSet<TKey>(original.Select(comparingKeys), null);
+
+            foreach (var item in incoming)
+                if (keys.Add(comparingKeys(item)))
+                    yield return item;
+        }
+    }
+}
